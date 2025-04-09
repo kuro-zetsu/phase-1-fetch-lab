c059acfe291267ba7469dad78c5d41ea9cb05e99
Reagan Nyauma <jreagan2525@gmail.com>
2025-04-09 07:47:30 +0300

    used fetch to get booknames from an api of ice and fire

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => {
+      const books = response.json();
+      return books;
+    })
+    .then(renderBooks);
 }
 
 function renderBooks(books) {
